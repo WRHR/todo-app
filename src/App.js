e927714fa6b343acf3d810c72cad7bd151870607
WRHR <wrhreeves@gmail.com>
2020-07-17 16:18:42 -0600

    Add TodoForm, creates new todo, adds todo to state and posts to backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {Component} from 'react';
 import './App.css';
 import TodoContaineer from './Components/TodoContainer'
+import TodoForm from './Components/TodoForm'
 
-const todosUrl = 'http://localhost:3000/todos'
+const todosUrl = 'http://localhost:3000/todos/'
 class App extends Component {
 
   state={
@@ -18,12 +19,39 @@ class App extends Component {
       .then(resp => resp.json())
       .then(todos => this.setState({todos}))
   }
+
+  addTodo = (newTodo) => {
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
+
+    fetch(todosUrl, {
+      method:'POST',
+      headers:{'Content-Type':'application/json'},
+      body: JSON.stringify(newTodo)
+    })
+  }
+
+  deleteTodo = (id) => {
+    let filtered = this.state.todos.filter(todo => todo.id !== id)
+
+    this.setState({
+      todos: filtered
+    })
+
+    fetch(todosUrl+id, {method:'DELETE'})
+  }
   
   render(){
     return (
       <div className="App">
         <h1>Todo App</h1>
-        <TodoContaineer todos={this.state.todos} />
+        <TodoForm addTodo={this.addTodo}/>
+        <TodoContaineer 
+          todos={this.state.todos} 
+          deleteTodo={this.deleteTodo}
+        />
+
       </div>
     );
   }
